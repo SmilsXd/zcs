f431cbd278241956432a84b4e3517e9db79bcf58
theclayton <clayton@flaresoftware.com>
2022-07-22 13:53:18 -0700

    add streets script

---
+++ scripts/nad-streets-units-mgrs/transform-csv.js
@@ -0,0 +1,182 @@
+const { DATA_FILE_LOC, LOG_INTERVAL } = require("./index");
+
+const fs = require("fs");
+const fsproms = require("fs/promises");
+const readline = require("readline");
+const zcs = require("../../index");
+const Mgrs = require("./mgrs");
+
+// Keep track of elapsed time
+let start = process.hrtime();
+
+let elapsedTime = function () {
+  const precision = 3; // 3 decimal places
+  const elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+  return process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms";
+};
+
+// zcs data
+const {
+  zipindexes, // format - "84088": 22
+} = zcs.internal;
+
+const stats = {
+  records: 0,
+};
+
+const ALPHA_NUMERIC = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
+
+function displayStats() {
+  const heapUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+
+  process.stdout.write(
+    `\rProcessed ${stats.records} records. Elapsed: ${elapsedTime()}. Heap used: ${Math.round(heapUsed * 100) / 100}MB`
+  );
+}
+
+/**
+ * Trims and limits whitespace to one and uppercase
+ * @param {String} str
+ * @returns
+ */
+function cleanUpStr(str) {
+  const trimmed = str.trim();
+
+  let resultStr = "";
+
+  // Only allow one white space char
+  for (let i = 0; i < trimmed.length; i++) {
+    const char = trimmed[i];
+
+    if (
+      (char === " " &&
+        resultStr.length > 0 &&
+        resultStr[resultStr.length - 1] === " ") || char ===
+      '\\'
+    ) {
+      continue;
+    } else {
+      resultStr += char;
+    }
+  }
+
+  return resultStr.toUpperCase();
+}
+
+/*
+Transform NAD to combine street names and units.
+Output columns:
+    street_name text,
+    street_number text, 
+    zip_code text,
+    zip_index text,
+    unit text,
+    mgrs text
+*/
+async function transformData() {
+  console.log(`Processing data from ${DATA_FILE_LOC}...`);
+
+  // remove data from any previous runs
+  await fsproms.writeFile(process.cwd() + "/dumps/streets.txt", "");
+
+  // open read and write streams
+  const writeStream = fs.createWriteStream(
+    process.cwd() + "/dumps/streets.txt"
+  );
+
+  const fileStream = fs.createReadStream(DATA_FILE_LOC);
+  const rl = readline.createInterface({
+    input: fileStream,
+  });
+
+  let lineNumber = 0;
+
+  rl.on("line", (line) => {
+    const columns = line.toString().split(",");
+
+    // Skip first row
+    if (lineNumber > 0) {
+      // Get zip index by finding 'Zip_Code' in zcs zipindexes
+      const zipCode = columns[7];
+      const zipIndex = zipindexes[zipCode];
+
+      // Combine street name: StN_PreMod[11], StN_PreDir[12], StN_PreTyp[13], StN_PreSep[14], StreetName[15], StN_PosTyp[16], StN_PosDir[17], StN_PosMod[18]
+      const streetName = cleanUpStr(
+        (columns[11] + "  " || "") +
+        (columns[12] + "  " || "") +
+        (columns[13] + "  " || "") +
+        (columns[14] + "  " || "") +
+        (columns[15] + "  " || "") +
+        (columns[16] + "  " || "") +
+        (columns[17] + "  " || "") +
+        (columns[18] + "  " || "")
+      );
+
+      // Combine street number: AddNum_Pre[19], Add_Number[20], AddNum_Suf[21]
+      const streetNumber = cleanUpStr(
+        (columns[19] + "  " || "") +
+        (columns[20] + "  " || "") +
+        (columns[21] + "  " || "")
+      );
+
+      // Building LandmkPart[22], LandmkName[23], Building[24], Floor[25], Unit[26], Room[27]
+      const unit = cleanUpStr(
+        (columns[22] + "  " || "") +
+        (columns[23] + "  " || "") +
+        (columns[24] ? "Building " + columns[24] + "  " : "") +
+        (columns[25] ? "Floor " + columns[25] + "  " : "") +
+        (columns[26] ? "Unit " + columns[26] + "  " : "") +
+        (columns[27] ? "Room " + columns[27] + "  " : "")
+      );
+
+      // Generate MGRS from lat and long: Longitude[30], Latitude[31]
+      const lattitude = Number(columns[30]);
+      const longitude = Number(columns[31]);
+
+      let mgrs = "";
+
+      if (!isNaN(lattitude) && !isNaN(longitude)) {
+        const tempMgrs = Mgrs.forward([lattitude, longitude]);
+
+        let i = 0
+        for (i; i< tempMgrs.length; i++) {
+          if (ALPHA_NUMERIC.indexOf(tempMgrs[i]) < 1) {
+            continue
+          }
+
+          mgrs += tempMgrs[i]
+        }
+      }
+
+      // Write out to dump file
+      writeStream.write(
+        (streetName || '') +
+        "," +
+        (streetNumber || '') +
+        "," +
+        (zipCode || '') +
+        "," +
+        (zipIndex || '') +
+        "," +
+        (unit || '') +
+        "," +
+        (mgrs || '') +
+        "\n"
+      );
+    }
+
+    lineNumber++;
+
+    // Display current stats
+    if (lineNumber % LOG_INTERVAL === 0) {
+      stats.records = lineNumber;
+      displayStats();
+    }
+  });
+
+  rl.on('close', () => {
+    console.log(`\ndone.\n`)
+  });
+}
+
+transformData();
---
+++ scripts/nad-streets-units-mgrs/write-output.js
@@ -0,0 +1,219 @@
+const { POSTGRESQL_CONN, LOG_INTERVAL } = require("./index");
+
+const fs = require("fs");
+const fsproms = require("fs/promises");
+const { Client } = require("pg");
+const client = new Client({ connectionString: POSTGRESQL_CONN });
+client.connect();
+
+const QueryStream = require("pg-query-stream");
+
+// Keep track of elapsed time
+let start = process.hrtime();
+
+let elapsedTime = function () {
+  const precision = 3; // 3 decimal places
+  const elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+  return process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms";
+};
+
+const stats = {
+  records: 0,
+  uniqueNames: 0,
+};
+
+function displayStats() {
+  const heapUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+
+  process.stdout.write(
+    `\rProcessed ${stats.records} records. Unique names: ${stats.uniqueNames
+    } Elapsed: ${elapsedTime()}. Heap used: ${Math.round(heapUsed * 100) / 100
+    }MB`
+  );
+}
+
+async function writeOut() {
+  console.log("\nWriting out data...");
+
+  // remove data from any previous runs
+  await fsproms.writeFile(process.cwd() + "/output/street-names.txt", "");
+  await fsproms.writeFile(process.cwd() + "/output/streets-index.txt", "");
+  await fsproms.writeFile(process.cwd() + "/output/street-data.txt", "");
+  await fsproms.writeFile(process.cwd() + "/output/mgrs.txt", "");
+
+  // open write streams
+  const streetNamesStream = fs.createWriteStream(
+    process.cwd() + "/output/street-names.txt"
+  );
+
+  const streetIndexStream = fs.createWriteStream(
+    process.cwd() + "/output/streets-index.txt"
+  );
+
+  const streetDataStream = fs.createWriteStream(
+    process.cwd() + "/output/street-data.txt"
+  );
+
+  streetDataStream.write(`[\n`);
+
+  const query = new QueryStream("select * from data order by name");
+  client.query(query);
+
+  let count = 0;
+  let index = 0;
+
+  let streetName = "";
+  let streetData = {};
+  let mgrsData = {};
+
+  for await (const street of query) {
+    if (count === 0) streetName = street.name;
+
+    if (street.name !== streetName) {
+      // Write street data to file
+      // TODO replace stringify with raw text output
+      streetDataStream.write(JSON.stringify(streetData) + "\n");
+
+      // Write unique street names to file
+      streetNamesStream.write(`${index === 0 ? "" : ","}"${streetName}"`);
+
+      // write street indexes to file
+      streetIndexStream.write(
+        `${index === 0 ? "" : ","}"${streetName}":${Number.parseInt(index)}`
+      );
+
+      index++;
+      streetName = street.name;
+      streetData = {};
+    }
+
+    if (!streetData[street.number]) {
+      streetData[street.number] = { [street.zip_index]: [] };
+    }
+
+    if (!streetData[street.number][street.zip_index]) {
+      streetData[street.number][street.zip_index] = []
+    }
+
+    // Add units
+    if (street.unit) {
+      // split unit type from unit number
+      const split = street.unit.split(" ");
+      let type = "";
+      let num = "";
+
+      if (split.length > 1) {
+        for (let k = 0; k < split.length; k++) {
+          if (k === split.length - 1) {
+            num = split[k];
+          } else {
+            type += split[k];
+          }
+        }
+      }
+
+      // Add 2D array of units
+      let addedToTypeArr = false;
+
+      for (
+        let k = 0;
+        k < streetData[street.number][street.zip_index].length;
+        k++
+      ) {
+        const checkType = streetData[street.number][street.zip_index][k][0];
+
+        if (checkType && checkType === type) {
+          streetData[street.number][street.zip_index][k][1].push(num);
+          addedToTypeArr = true;
+          break;
+        }
+      }
+
+      if (!addedToTypeArr) {
+        streetData[street.number][street.zip_index].push([type, []]);
+
+        streetData[street.number][street.zip_index][
+          streetData[street.number][street.zip_index].length - 1
+        ][1].push(num);
+      }
+
+      //////////
+      // MGRS DATA
+      // Split into 4 parts
+      let mgrsParts = ["", "", "", ""];
+
+      let j;
+      for (j = 0; j < street.mgrs.length; j++) {
+        if (j < 3) {
+          mgrsParts[0] += street.mgrs[j];
+        } else if (j >= 3 && j < 5) {
+          mgrsParts[1] += street.mgrs[j];
+        } else if (j >= 5 && j < 10) {
+          mgrsParts[2] += street.mgrs[j];
+        } else if (j >= 10 && j < 15) {
+          mgrsParts[3] += street.mgrs[j];
+        }
+      }
+
+      if (!mgrsData[mgrsParts[0]]) {
+        mgrsData[mgrsParts[0]] = {};
+      }
+
+      if (!mgrsData[mgrsParts[0]][mgrsParts[1]]) {
+        mgrsData[mgrsParts[0]][mgrsParts[1]] = {};
+      }
+
+      if (!mgrsData[mgrsParts[0]][mgrsParts[1]][mgrsParts[2]]) {
+        mgrsData[mgrsParts[0]][mgrsParts[1]][mgrsParts[2]] = {};
+      }
+
+      if (!mgrsData[mgrsParts[0]][mgrsParts[1]][mgrsParts[2]][mgrsParts[3]]) {
+        mgrsData[mgrsParts[0]][mgrsParts[1]][mgrsParts[2]][mgrsParts[3]] = [
+          index,
+        ];
+      } else {
+        mgrsData[mgrsParts[0]][mgrsParts[1]][mgrsParts[2]][mgrsParts[3]].push(
+          index
+        );
+      }
+    }
+
+    count++;
+
+    // Display current stats
+    if (count % LOG_INTERVAL === 0) {
+      stats.records = count;
+      stats.uniqueNames = index + 1;
+      displayStats();
+    }
+  }
+
+  // Write street data to file
+  // TODO replace stringify with raw text output
+  streetDataStream.write(JSON.stringify(streetData) + "\n");
+
+  // Write unique street names to file
+  streetNamesStream.write(`${index === 0 ? "" : ","}"${streetName}"`);
+
+  // write street indexes to file
+  streetIndexStream.write(
+    `${index === 0 ? "" : ","}"${streetName}":${Number.parseInt(index)}`
+  );
+
+  streetDataStream.write(`]`);
+
+  const mgrsStream = fs.createWriteStream(process.cwd() + "/output/mgrs.txt");
+  // May need to stream this somehow
+  mgrsStream.write(JSON.stringify(mgrsData));
+  mgrsStream.end();
+
+  streetNamesStream.end();
+  streetIndexStream.end();
+  streetDataStream.end();
+
+  client.end();
+
+  console.log("\n\ndone.");
+}
+
+writeOut();
